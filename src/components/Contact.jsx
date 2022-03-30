5951e81ca5dd6458a8f4de2f5a99ccd4d7416162
Anderson Vallejo <vallejoanderson@gmail.com>
2022-03-30 15:18:39 -0500

    fix: broken links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,10 +13,14 @@ export const Contact = () => {
 				<img className="rounded-full border-2 w-40 mx-auto" src={ profile } alt="profile picture" />
 				<h2 className="text-slate-50 text-base md:text-xl text-center mt-8 tracking-widest">ANDERSON VALLEJO</h2>
 				<div className="flex gap-8 justify-center mt-4">
-					<img className="cursor-pointer" src={ linkedin }/>
-					<img className="cursor-pointer" src={ twitter }/>
+					<a href='https://www.linkedin.com/in/anderson-vallejo-643367a2/' target='_blank'>
+						<img className="cursor-pointer" src={ linkedin }/>
+					</a>
+					<a href='https://twitter.com/vallejoaa' target='_blank'>
+						<img className="cursor-pointer" src={ twitter }/>
+					</a>
 				</div>
-				<a href="https://drive.google.com/file/d/1OXjHTYrVEANlgE4dmfHZjEn-gyaFA04a/view?usp=sharing" target="_blannk">
+				<a href="https://drive.google.com/file/d/1yPhKI5JbAZDxzHmBHN-MqrI0Z5SeUMl1/view?usp=sharing" target="_blank">
 					<p className="text-slate-50 border-2 text-center px-16 py-4 mt-24 text-base md:text-xl cursor-pointer hover:bg-white hover:text-black tracking-widest">DOWNLOAD CV</p>
 				</a>
 			</div>
